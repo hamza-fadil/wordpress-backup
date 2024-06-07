17eeacaeaf8f1aac2e415c333840d9bd0c8e55fc
Hamza FADIL <h.fadil7@gmail.com>
2024-06-07 20:34:57 +0200

    Optimize compression with pigz and add more error checks

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
-if ! [ -f backup-cron ]
-then
+if ! [ -f /etc/crontabs/root ]; then
   echo "Creating cron entry to start backup at: $BACKUP_TIME"
-  # Note: Must use tabs with indented 'here' scripts.
-  cat <<-EOF >> backup-cron
+  
+  cat <<-EOF > /etc/crontabs/root
 MYSQL_ENV_MYSQL_HOST=$MYSQL_ENV_MYSQL_HOST
 MYSQL_ENV_MYSQL_USER=$MYSQL_ENV_MYSQL_USER
 MYSQL_ENV_MYSQL_DATABASE=$MYSQL_ENV_MYSQL_DATABASE
 MYSQL_ENV_MYSQL_PASSWORD=$MYSQL_ENV_MYSQL_PASSWORD
 EOF
 
-  if [[ $CLEANUP_OLDER_THAN ]]
-  then
-    echo "CLEANUP_OLDER_THAN=$CLEANUP_OLDER_THAN" >> backup-cron
+  # Add cleanup variable if it exists
+  if [ -n "$CLEANUP_OLDER_THAN" ]; then
+    echo "CLEANUP_OLDER_THAN=$CLEANUP_OLDER_THAN" >> /etc/crontabs/root
   fi
-  echo "$BACKUP_TIME backup > /backup.log" >> backup-cron
 
-  crontab backup-cron
+  # Add backup job to cron
+  echo "$BACKUP_TIME /bin/backup > /backup.log 2>&1" >> /etc/crontabs/root
+
+  crontab /etc/crontabs/root
 fi
 
 echo "Current crontab:"
